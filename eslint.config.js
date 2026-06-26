9182ad27f40acf7724ab68424e7207884aa74c8c
Le Vivilet <levivilet@pm.me>
2026-06-26 16:40:24 +0200

    feature: update lint config (#12)

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,11 @@ import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...config.recommendedRegex,
   ...actions.default,
   {
     rules: {
       'github-actions/ci-versions': 'off',
-      'unicorn/no-top-level-side-effects': 'off',
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-      'no-useless-escape': 'off',
     },
   },
 ]
