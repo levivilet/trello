5e8c22ef4d78a314ee238ac0f99d5882e7f8d68e
Le Vivilet <levivilet@pm.me>
2026-07-02 23:34:19 +0200

    move server into package (#14)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -30,7 +30,7 @@ function updateDependencies {
 cd packages/build           && updateDependencies && cd ../../ &&
 cd packages/e2e             && updateDependencies && cd ../../ &&
 cd packages/extension       && updateDependencies && cd ../../ &&
-cd packages/integration     && updateDependencies && cd ../../ &&
+cd packages/server          && updateDependencies && cd ../../ &&
 
 echo "Great Success!"
 
