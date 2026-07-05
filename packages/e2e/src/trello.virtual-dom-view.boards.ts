d21f2b8c4208c4f63cbed0260396081a12b75fa1
Le Vivilet <levivilet@pm.me>
2026-07-05 09:20:12 +0200

    fix: include Main parameter to close all editors in test setup (#63)

--- packages/e2e/src/trello.virtual-dom-view.boards.ts
+++ packages/e2e/src/trello.virtual-dom-view.boards.ts
@@ -9,7 +9,8 @@ import {
 export const name = 'trello.virtual-dom-view.boards'
 export const skip = true
 
-export const test: Test = async ({ Command, expect, Locator }) => {
+export const test: Test = async ({ Command, expect, Locator, Main }) => {
+  await Main.closeAllEditors()
   const boards = createBoards(1)
   await useMockDataAndShowTrello(Command, createMockData(boards))
   await connectWithCredentials({ expect, Locator })
