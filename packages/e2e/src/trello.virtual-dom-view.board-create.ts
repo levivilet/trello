8b31a1eeb357a5cd55f5e4e329f319dff64cfd24
Le Vivilet <levivilet@pm.me>
2026-07-10 08:10:53 +0000

    fix

--- packages/e2e/src/trello.virtual-dom-view.board-create.ts
+++ packages/e2e/src/trello.virtual-dom-view.board-create.ts
@@ -1,5 +1,4 @@
 // @ts-nocheck
-/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/prefer-readonly-parameter-types */
 import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'trello.virtual-dom-view.list-create'
