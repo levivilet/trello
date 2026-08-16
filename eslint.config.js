2ee1ec45c05e2a710f6ae680e0b7f23e8ec3a13f
Le Vivilet <levivilet@pm.me>
2026-08-16 13:20:19 +0000

    feature: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import * as config from '@lvce-editor/eslint-config'
+import { defineConfig } from 'eslint/config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedRegex,
   ...config.recommendedTsconfig,
@@ -46,4 +47,4 @@ export default [
       'e2e/no-imports': 'off',
     },
   },
-]
+])
